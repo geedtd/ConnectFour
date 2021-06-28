d424aec65b5b3e044067895564722fed4029e845
geedtd <grcazares@gmail.com>
2021-06-28 08:48:54 -0700

    Adding const, elements, and variables

---
+++ app.js
@@ -0,0 +1,14 @@
+//HTML Elements
+const status = document.querySelector('.status')
+const resetBtn = document.querySelector('.reset')
+
+//game constants  
+const redPuck = '0' //will change to background-color changing event
+const yellowPuck = '1' //will change to background-color changing event
+//gaem variables 
+let gameWon = false;
+let player1Turn = true;
+//game listener
+
+//functions
+
